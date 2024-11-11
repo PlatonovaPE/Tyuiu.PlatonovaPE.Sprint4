bc07c327956686d14ee55c43ad9cc5529138f3af
Полина Платонова <platonovaPE@outlook.com>
2024-11-11 23:17:27 +0500

    Task3

---
+++ Tyuiu.PlatonovaPE.Sprint4.Task3.V29.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.PlatonovaPE.Sprint4.Task3.V29.Lib
+{
+    public class DataService : ISprint4Task3V29
+    {
+        public int Calculate(int[,] array)
+        {
+            int p = 1;
+            
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (j == 0)
+                    {
+                        p *= array[i, j];
+                    }
+                }
+            }
+
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.PlatonovaPE.Sprint4.Task3.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using Tyuiu.PlatonovaPE.Sprint4.Task3.V29.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* Исходные данные                                                              *");
+Console.WriteLine("********************************************************************************");
+
+int[,] array = new int[5, 5] { { 9, 9, 8, 6, 9 },
+                                          { 5, 8, 8, 8, 7 },
+                                          { 6, 5, 9, 7, 9 },
+                                          { 7, 7, 9, 7, 8 },
+                                          { 8, 5, 8, 5, 5 } };
+
+Console.WriteLine("массив:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(array);
+Console.WriteLine(res);
+Console.ReadKey();
+
+
+
+
+
+
